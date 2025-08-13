505b37be2f652f7b420836df5e0c79a117672c7b
marcusvco <marcuscarvalhodeoliveira@gmail.com>
2025-08-13 20:02:36 -0300

    feat: get repo pull requests, releases, closed issues

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ api_url = "https://api.github.com"
 
 headers = {"Authorization": f"token {token}"}
 
-def get_popular_repos(keyword, num_repos):
-    url = f"{api_url}/search/repositories?q={keyword}&sort=stars&order=desc&per_page={num_repos}"
+def get_popular_repos(num_repos):
+    url = f"{api_url}/search/repositories?q=stars:>80000&sort=stars&order=desc&per_page={num_repos}"
     response = requests.get(url, headers=headers)
     
     if response.status_code == 200:
@@ -23,30 +23,56 @@ def get_repo_details(owner, repo):
     else:
         raise Exception(f"Failed to fetch repository details: {response.status_code}")
 
-def collect_and_print_repo_info(repos):
-    for repo in repos:
-        owner = repo["owner"]["login"]
-        repo_name = repo["name"]
-        repo_details = get_repo_details(owner, repo_name)
+def get_repo_pull_requests(owner, repo):
+    url = f"{api_url}/repos/{owner}/{repo}/pulls"
+    response = requests.get(url, headers=headers)
 
-        print(f"Repository: {repo_name}")
-        print(f"Stars: {repo_details['stargazers_count']}")
-        print("================================")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Failed to fetch repository pull requests: {response.status_code}")
+
+def get_repo_releases(owner, repo):
+    url = f"{api_url}/repos/{owner}/{repo}/releases"
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Failed to fetch repository releases: {response.status_code}")
+
+def get_repo_closed_issues(owner, repo):
+    url = f"{api_url}/repos/{owner}/{repo}/issues?state=closed"
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Failed to fetch repository closed issues: {response.status_code}")
 
 def print_repo_infos(repos):
     for repo in repos:
+        owner = repo["owner"]["login"]
+        repo_name = repo["name"]
+        repo_pull_requests = get_repo_pull_requests(owner, repo_name)
+        repo_releases = get_repo_releases(owner, repo_name)
+        closed_issues = get_repo_closed_issues(owner, repo_name)
+
         print(f"Repository: {repo['name']}")
         print(f"Stars: {repo['stargazers_count']}")
         print(f"Created at: {repo['created_at']}")
         print(f"Issues count: {repo['open_issues_count']}")
+        print(f"Total pull requests: {len(repo_pull_requests)}")
+        print(f"Total releases: {len(repo_releases)}")
+        print(f"Language: {repo['language']}")
+        print(f"Closed issues: {len(closed_issues)}")
         print("================================")
 
 if __name__ == "__main__":
-    keyword = "microservices"
-    num_repos = 10
+    num_repos = 100
 
     try:
-        popular_repos = get_popular_repos(keyword=keyword, num_repos=num_repos)
+        popular_repos = get_popular_repos(num_repos=num_repos)
         print_repo_infos(popular_repos)
     except Exception as e:
         print(e)
